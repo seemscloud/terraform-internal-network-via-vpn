43417da8daa58bf09e0890bf85f062cd8a0a4177
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-08-15 17:53:58 +0200

    adsf

--- main.tf
+++ main.tf
@@ -14,6 +14,8 @@ module "aaa_subnet_aaa" {
   ip_cidr_range = var.aaa_subnet_aaa-ip_cidr_range
   region        = var.aaa_subnet_aaa-region
   network       = module.aaa_vpc_aaa.name
+
+  depends_on = [module.aaa_vpc_aaa]
 }
 
 module "aaa_subnet_bbb" {
@@ -23,6 +25,8 @@ module "aaa_subnet_bbb" {
   ip_cidr_range = var.aaa_subnet_bbb-ip_cidr_range
   region        = var.aaa_subnet_bbb-region
   network       = module.aaa_vpc_aaa.name
+
+  depends_on = [module.aaa_vpc_aaa]
 }
 
 
@@ -33,4 +37,6 @@ module "aaa_subnet_ccc" {
   ip_cidr_range = var.aaa_subnet_ccc-ip_cidr_range
   region        = var.aaa_subnet_ccc-region
   network       = module.aaa_vpc_aaa.name
+
+  depends_on = [module.aaa_vpc_aaa]
 }
